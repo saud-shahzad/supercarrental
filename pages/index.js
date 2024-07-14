5a7f684443667b46f9c353e50535ec67eac85ed6
saud-shahzad <57951076+saud-shahzad@users.noreply.github.com>
2024-07-14 19:37:50 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Supercar Rental Dubai: Experience Luxury on Wheels | Book Now</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
